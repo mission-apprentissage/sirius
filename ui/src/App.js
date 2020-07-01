d6ec9565f6d56fafc3dd6c5eb2abae2e5b0eebb2
Benoît Guérout <bguerout@gmail.com>
2020-07-01 15:07:09 +0200

    Add global styles and flexbox grid

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Grid, Row, Col, Box } from "./common/FlexboxGrid";
 
 function App() {
-  return <div className="App">Sirius</div>;
+  return (
+    <div className="App">
+      <Grid>
+        <Row around={true}>
+          <Col>Sirius</Col>
+          <Col>
+            <Box between={true}>
+              <div>titi</div>
+              <div>tata</div>
+            </Box>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import GlobalStyle from "./common/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
