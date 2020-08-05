7807ad0bf9969348d650ea5805791a38dcdbd69e
Benoît Guérout <bguerout@gmail.com>
2020-08-05 11:02:31 +0200

    Add resent questionnaire script

---
+++ server/src/questionnaires/jobs/resendQuestionnaires/resendQuestionnaires.js
@@ -0,0 +1,35 @@
+const { oleoduc, writeObject } = require("oleoduc");
+const { delay } = require("../../../core/asyncUtils");
+
+module.exports = async (db, logger, questionnaires, options = {}) => {
+  let type = "finAnnee";
+  let limit = options.limit || 1;
+  let stats = {
+    total: 0,
+    sent: 0,
+    failed: 0,
+  };
+
+  await oleoduc(
+    db.collection("contrats").find({ unsubscribe: false, "questionnaires.nbEmailsSent": { $lt: 2 } }),
+    writeObject(
+      async (contrat) => {
+        try {
+          let results = contrat.questionnaires.filter(({ status }) => status && status !== "closed");
+          if (results.length > 0 && ++stats.total <= limit) {
+            logger.info(`Resending questionnaire ${type} to ${contrat.apprenti.email}`);
+            await Promise.all(results.map((q) => questionnaires.send(q.token)));
+            await delay(100);
+            stats.sent += results.length;
+          }
+        } catch (e) {
+          logger.error(e);
+          stats.failed++;
+        }
+      },
+      { parallel: 2 }
+    )
+  );
+
+  return stats;
+};
---
+++ server/test/integration/questionnaire/resendQuestionnaires-test.js
@@ -0,0 +1,120 @@
+const assert = require("assert");
+const { omit } = require("lodash");
+const integrationTests = require("../utils/integrationTests");
+const logger = require("../utils/fakeLogger");
+const createFakeMailer = require("../utils/fakeMailer");
+const { newContrat } = require("../utils/fixtures");
+const resendQuestionnaires = require("../../../src/questionnaires/jobs/resendQuestionnaires/resendQuestionnaires");
+
+integrationTests(__filename, ({ getComponents }) => {
+  it("Vérifie qu'on peut renvoyer un questionnaire", async () => {
+    let emails = [];
+    let { db, questionnaires } = await getComponents({
+      mailer: createFakeMailer({ calls: emails }),
+    });
+    await db.collection("contrats").insertOne(
+      newContrat({
+        apprenti: {
+          email: "test@domain.com",
+        },
+        questionnaires: [{ type: "finAnnee", nbEmailsSent: 1, status: "sent", token: "12345", reponses: [] }],
+      })
+    );
+
+    let stats = await resendQuestionnaires(db, logger, questionnaires);
+
+    assert.deepStrictEqual(stats, {
+      total: 1,
+      sent: 1,
+      failed: 0,
+    });
+
+    let found = await db.collection("contrats").findOne();
+    let questionnaire = found.questionnaires[0];
+    let token = questionnaire.token;
+    assert.ok(questionnaire.sentDate);
+    assert.deepStrictEqual(omit(questionnaire, ["sentDate"]), {
+      type: "finAnnee",
+      status: "sent",
+      token,
+      nbEmailsSent: 2,
+      reponses: [],
+    });
+
+    //Check emails
+    assert.strictEqual(emails.length, 1);
+    let email = emails[0];
+    assert.strictEqual(email.from, "sirius@apprentissage.beta.gouv.fr");
+    assert.strictEqual(email.to, "test@domain.com");
+    assert.strictEqual(email.subject, "Que pensez-vous de votre formation CAP Boucher ?");
+    assert.ok(email.html.lastIndexOf("Donnez votre avis en 3 minutes") !== -1);
+    assert.ok(email.html.lastIndexOf(`http://localhost/questionnaires/${token}`) !== -1);
+    assert.strictEqual(emails.length, 1);
+  });
+
+  it("Vérifie qu'on peut renvoyer qu'une seule fois un questionnaire", async () => {
+    let emails = [];
+    let { db, questionnaires } = await getComponents({
+      mailer: createFakeMailer({ calls: emails }),
+    });
+    await db.collection("contrats").insertOne(
+      newContrat({
+        apprenti: {
+          email: "test@domain.com",
+        },
+        questionnaires: [{ type: "finAnnee", nbEmailsSent: 2, status: "sent", token: "12345", reponses: [] }],
+      })
+    );
+
+    let stats = await resendQuestionnaires(db, logger, questionnaires);
+
+    assert.deepStrictEqual(stats, {
+      total: 0,
+      sent: 0,
+      failed: 0,
+    });
+    assert.strictEqual(emails.length, 0);
+  });
+
+  it("Vérifie que les questionnaires terminés ne sont pas renvoyés", async () => {
+    let emails = [];
+    let { db, questionnaires } = await getComponents({
+      mailer: createFakeMailer({ calls: emails }),
+    });
+    await db.collection("contrats").insertOne(
+      newContrat({
+        apprenti: {
+          email: "test@domain.com",
+        },
+        questionnaires: [{ type: "finAnnee", nbEmailsSent: 1, status: "closed", token: "12345", reponses: [] }],
+      })
+    );
+
+    let stats = await resendQuestionnaires(db, logger, questionnaires);
+
+    assert.strictEqual(emails.length, 0);
+    assert.deepStrictEqual(stats, {
+      total: 0,
+      sent: 0,
+      failed: 0,
+    });
+  });
+
+  it("Vérifie que lors d'un renvoi le statut est préservé", async () => {
+    let { db, questionnaires } = await getComponents();
+    await db.collection("contrats").insertOne(
+      newContrat({
+        apprenti: {
+          email: "test@domain.com",
+        },
+        questionnaires: [{ type: "finAnnee", nbEmailsSent: 1, status: "open", token: "12345", reponses: [] }],
+      })
+    );
+
+    await resendQuestionnaires(db, logger, questionnaires);
+
+    let found = await db.collection("contrats").findOne();
+    let questionnaire = found.questionnaires[0];
+    assert.strictEqual(questionnaire.status, "open");
+  });
+});
