7e937a0cf33d6c54fbdea1bbd4f90ba44d5ea48a
Benoit Guérout <bguerout@gmail.com>
2020-08-31 13:39:51 +0200

    Set status=error when questionnaire cannot be sent (#25)

--- server/src/contrats/csv/cfa/utils/buildContrat.js
+++ server/src/contrats/csv/cfa/utils/buildContrat.js
@@ -15,7 +15,7 @@ module.exports = (data) => {
     apprenti: {
       prenom: data.prenom_apprenti,
       nom: data.nom_apprenti,
-      email: data.email_apprenti,
+      email: data.email_apprenti.replace(/ /g, ""),
       telephones: {
         fixe: sanitize(data.telephone_apprenti),
         portable: sanitize(data.portable_apprenti),
