9e233dc55b62885a7330e2d5a6239c082de47cb3
Benoît Guérout <bguerout@gmail.com>
2020-09-30 21:07:47 +0200

    Add migration script

--- server/src/db/migration/convertContratsIntoApprenti.js
+++ server/src/db/migration/convertContratsIntoApprenti.js
@@ -1,19 +1,23 @@
 const { getNbModifiedDocuments } = require("../../core/mongoUtils");
 const { oleoduc, transformObject, writeObject } = require("oleoduc");
+const createIndexes = require("../indexes/createIndexes");
+const dropIndexes = require("../indexes/dropIndexes");
 
 module.exports = async (db) => {
   let stats = {
     updated: 0,
   };
 
+  await dropIndexes(db);
   await oleoduc(
     db.collection("contrats").find(),
     transformObject((contrat) => {
       return {
-        ...contrat.apprenti,
+        _id: contrat._id,
         creationDate: contrat.creationDate,
         cohorte: contrat.cohorte,
         unsubscribe: contrat.unsubscribe,
+        ...contrat.apprenti,
         contrats: [
           {
             questionnaires: contrat.questionnaires,
@@ -25,16 +29,14 @@ module.exports = async (db) => {
         ],
       };
     }),
-    writeObject(
-      async (contrat) => {
-        let results = await db.collection("contrats").replaceOne({ _id: contrat._id }, contrat);
-        stats.updated += getNbModifiedDocuments(results);
-      },
-      { parallel: 2 }
-    )
+    writeObject(async (apprenti) => {
+      let results = await db.collection("contrats").replaceOne({ _id: apprenti._id }, apprenti);
+      stats.updated += getNbModifiedDocuments(results);
+    })
   );
 
   await db.collection("contrats").rename("apprentis");
+  await createIndexes(db);
 
   return stats;
 };
