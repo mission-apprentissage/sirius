ba08aea40f95ec7296562de1dce08989b5240599
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-02-13 16:22:23 +0100

    feat(campagne): add getAll route & controller

--- server/src/campagnes/campagnesHttp.js
+++ server/src/campagnes/campagnesHttp.js
@@ -3,14 +3,22 @@ const tryCatch = require("../core/http/tryCatchMiddleware");
 const validator = require("../core/http/validatorMiddleware");
 const createCampagneSchema = require("./validators");
 
-const campagnesHttp = ({ campagnes }) => {
+const campagnesHttp = ({ campagnesController }) => {
   const router = express.Router();
+
+  router.get(
+    "/api/campagnes/",
+    tryCatch(async (req, res) => {
+      const campagnes = await campagnesController.getAll();
+      return res.status(200).json(campagnes);
+    })
+  );
 
   router.post(
     "/api/campagnes/",
     validator(createCampagneSchema),
     tryCatch(async (req, res) => {
-      const campagne = await campagnes.create(req.body);
+      const campagne = await campagnesController.create(req.body);
       if (campagne.result.ok === 1) {
         return res.status(201).json(campagne.ops[0]);
       } else {
--- server/src/core/campagnes.js
+++ server/src/core/campagnes.js
@@ -1,5 +1,8 @@
 module.exports = (db) => {
   return {
+    getAll: () => {
+      return db.collection("campagnes").find({}).toArray();
+    },
     create: ({ nomCampagne, cfa, formation, startDate, endDate, questionnaire, questionnaireUI }) => {
       return db
         .collection("campagnes")
