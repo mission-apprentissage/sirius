89465a007d8c520bfcfaf25eb5053c05010e8e05
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-02-28 10:23:24 +0100

    fix(cli): fix create-user cli

--- server/src/db/createUser.js
+++ server/src/db/createUser.js
@@ -1,29 +1,10 @@
 const User = require("../models/user.model");
-const { getToken, getRefreshToken } = require("../utils/authenticate.utils");
 
 module.exports = async (username, password, firstName, lastName) => {
-  User.register(new User({ username: username }), password, (err, user) => {
-    if (err) {
-      console.log(err);
-    } else {
-      user.firstName = firstName;
-      user.lastName = lastName || "";
-
-      const token = getToken({ _id: user._id });
-      const refreshToken = getRefreshToken({ _id: user._id });
-
-      user.refreshToken.push({ refreshToken });
-
-      user.save((err, user) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("User created");
-          console.log("User: ", user);
-          console.log("Token: ", token);
-          console.log("Refresh token: ", refreshToken);
-        }
-      });
-    }
-  });
+  try {
+    const newUser = await User.register(new User({ username, firstName, lastName }), password);
+    console.log(newUser);
+  } catch (err) {
+    console.log(err);
+  }
 };
