2b76f7459c5fe36af2c9163d743be119baf2b0b4
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-04-13 11:30:27 +0200

    feat(temoignages): extract modal in component

---
+++ ui/src/temoignages/Components/TemoignagesModal.js
@@ -0,0 +1,44 @@
+import {
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
+
+const TemoignagesModal = ({ question, responses }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  if (!responses.length) return null;
+
+  return (
+    <>
+      <CardFooter>
+        <Button onClick={onOpen}>Voir plus</Button>
+      </CardFooter>
+      <Modal onClose={onClose} isOpen={isOpen} size="xl" isCentered>
+        <ModalOverlay bg="blackAlpha.300" />
+        <ModalContent>
+          <ModalHeader p={4}>{question}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {responses?.map((response, index) => (
+              <Text key={index} fontSize="xl" pl={4} mb={6}>
+                {response}
+              </Text>
+            ))}
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default TemoignagesModal;
