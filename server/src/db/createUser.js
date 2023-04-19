538349075e30e7161bb14d701aeec38c65478485
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-04-19 16:03:40 +0200

    feat(user): remove console log when creating user

--- server/src/db/createUser.js
+++ server/src/db/createUser.js
@@ -2,8 +2,7 @@ const User = require("../models/user.model");
 
 module.exports = async (username, password, firstName, lastName) => {
   try {
-    const newUser = await User.register(new User({ username: username.toLowerCase(), firstName, lastName }), password);
-    console.log(newUser);
+    return User.register(new User({ username: username.toLowerCase(), firstName, lastName }), password);
   } catch (err) {
     console.log(err);
   }
