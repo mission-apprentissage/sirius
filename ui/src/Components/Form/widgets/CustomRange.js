2e954706565fa1fe08d4ed0acf060ecfb63acb36
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-05-03 19:22:41 +0200

    feat(customRange): enhance custom range

--- ui/src/Components/Form/widgets/CustomRange.js
+++ ui/src/Components/Form/widgets/CustomRange.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Slider,
   SliderTrack,
   SliderThumb,
   SliderMark,
+  SliderFilledTrack,
   Box,
   useBreakpoint,
   FormLabel,
+  Badge,
+  Text,
 } from "@chakra-ui/react";
 
 const CustomRange = (props) => {
@@ -14,32 +17,49 @@ const CustomRange = (props) => {
   const isMobile = breakpoint === "base";
   const savedValue = props.schema.enum.indexOf(props.value) || null;
 
+  useEffect(() => {
+    if (!props.value) props.onChange(props.schema.enum[0]);
+  }, []);
+
   return (
     <Box mx="5">
-      <FormLabel as="legend" fontSize="lg" fontWeight="semibold">
-        {props.schema.title}
+      <FormLabel
+        as="legend"
+        fontSize="2xl"
+        fontWeight="semibold"
+        color="orange.500"
+        requiredIndicator={
+          <Badge bgColor="orange.500" color="white" ml="2">
+            *
+          </Badge>
+        }
+      >
+        {props.label}
       </FormLabel>
+      <Text fontSize="xs" color="orange.900">
+        (Déplace le curseur sur l’émoji qui se rapproche le plus de ton ressenti)
+      </Text>
       <Box pt={6} pb={isMobile ? 6 : 12} w={isMobile ? "100%" : "90%"} m="auto">
         <Slider
           id="slider"
           defaultValue={savedValue}
           min={0}
           max={props.schema.enum.length - 1}
-          colorScheme="purple"
+          colorScheme="orange"
           w="100%"
           orientation={isMobile ? "vertical" : "horizontal"}
           height={isMobile ? "300px" : "inherit"}
-          onChangeEnd={(value) => props.onChange(props.schema.enum[value])}
+          onChange={(value) => props.onChange(props.schema.enum[value])}
         >
           {props.schema.enum.map((elem, index) => {
             return (
               <SliderMark
                 key={index}
                 value={index}
-                mt={isMobile ? "10px" : "3"}
+                mt={isMobile ? "10px" : "4"}
                 fontSize="sm"
-                width={isMobile ? "70%" : "150px"}
-                ml={isMobile ? "10px" : "-75px"}
+                width={isMobile ? "70%" : "120px"}
+                ml={isMobile ? "10px" : "-60px"}
                 maxW={isMobile ? "100%" : 100 / (props.schema.enum.length - 1) + "%"}
                 textAlign={isMobile ? "left" : "center"}
                 bottom={
@@ -48,13 +68,24 @@ const CustomRange = (props) => {
                     : "inherit"
                 }
                 minHeight={isMobile ? "37px" : "inherit"}
+                bgColor={
+                  index === props.schema.enum.indexOf(props.value) ? "orange.500" : "orange.50"
+                }
+                color={index === props.schema.enum.indexOf(props.value) ? "white" : "orange.800"}
+                borderRadius="md"
+                fontWeight="semibold"
+                py="1"
               >
                 {elem}
               </SliderMark>
             );
           })}
-          <SliderTrack bgColor="purple.200" left={isMobile ? "0!important" : "inherit"} />
-          <SliderThumb bgColor="purple.400" left={isMobile ? "0!important" : "inherit"} />
+          <SliderTrack colorScheme="orange">
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb left={isMobile ? "0!important" : "inherit"} fontSize="xl">
+            {props.uiSchema.emojis[props.schema.enum.indexOf(props.value)]}
+          </SliderThumb>
         </Slider>
       </Box>
     </Box>
