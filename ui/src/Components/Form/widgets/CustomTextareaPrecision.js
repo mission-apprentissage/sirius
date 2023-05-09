4adc23255dfb43608b20acb42817fafdd75d72bb
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-05-09 17:30:34 +0200

    feat(CustomTextareaPrecision): add padding

--- ui/src/Components/Form/widgets/CustomTextareaPrecision.js
+++ ui/src/Components/Form/widgets/CustomTextareaPrecision.js
@@ -1,18 +1,21 @@
-import { FormControl, Textarea } from "@chakra-ui/react";
+import { FormControl, Textarea, Box } from "@chakra-ui/react";
 
 const CustomTextareaPrecision = (props) => {
   return (
-    <FormControl id={props.id}>
-      <Textarea
-        onChange={(e) => props.onChange(e.target.value)}
-        placeholder="Tu peux préciser si tu le souhaites !"
-        borderColor="purple.50"
-        bgColor="gray.200"
-        focusBorderColor="purple.50"
-        mt="10px"
-        value={props.value}
-      />
-    </FormControl>
+    <Box mx="5">
+      <FormControl id={props.id} width="100%">
+        <Textarea
+          onChange={(e) => props.onChange(e.target.value)}
+          placeholder="Tu peux préciser si tu le souhaites !"
+          borderColor="purple.50"
+          bgColor="gray.200"
+          focusBorderColor="purple.50"
+          mt="10px"
+          value={props.value}
+          size="md"
+        />
+      </FormControl>
+    </Box>
   );
 };
 
