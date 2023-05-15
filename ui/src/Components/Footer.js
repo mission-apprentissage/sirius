bd3ccdee6ca05c7c820d5acfa067b68d06fd52f0
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-05-15 16:45:38 +0200

    feat(footer): add footer

---
+++ ui/src/Components/Footer.js
@@ -0,0 +1,102 @@
+import { Box, Image, Text, Link, useBreakpoint } from "@chakra-ui/react";
+import gouv from "../assets/images/gouv.svg";
+const Footer = () => {
+  const breakpoint = useBreakpoint({ ssr: false });
+  const isMobile = breakpoint === "base";
+  return (
+    <Box w="100%" display="flex" flexDirection="column" px={isMobile ? "2" : "28"}>
+      <Box
+        display="flex"
+        flexDirection={isMobile ? "column" : "row"}
+        alignItems="center"
+        justifyContent="flex-start"
+        borderBottom="1px solid #1A365D"
+        w="100%"
+        m={isMobile ? "auto" : "0"}
+        pb="4"
+      >
+        <Image src={gouv} alt="" w="150px" />
+        <Text color="purple.900">
+          <Link
+            href="https://travail-emploi.gouv.fr/"
+            target="_blank"
+            rel="noreferrer"
+            textDecoration="underline"
+            fontWeight="semibold"
+            color="purple.900"
+          >
+            Sirius
+          </Link>{" "}
+          est proposé par les services suivants :
+        </Text>
+        <Box
+          display="flex"
+          flexDirection={isMobile ? "column" : "row"}
+          justifyContent={isMobile ? "flex-start" : "space-between"}
+          w="100%"
+          m={isMobile ? "auto" : "0"}
+          mt={isMobile ? "2" : "0"}
+          px={isMobile ? "4" : "0"}
+        >
+          <Box mx={isMobile ? "0" : "1"}>
+            <Link
+              href="https://www.onisep.fr/"
+              target="_blank"
+              rel="noreferrer"
+              textDecoration="underline"
+              fontWeight="semibold"
+              color="purple.900"
+            >
+              onisep.fr
+            </Link>
+          </Box>
+          <Box mx={isMobile ? "0" : "1"}>
+            <Link
+              href="https://beta.gouv.fr/startups/?incubateur=mission-inserjeunes"
+              target="_blank"
+              rel="noreferrer"
+              textDecoration="underline"
+              fontWeight="semibold"
+              color="purple.900"
+            >
+              Mission interministérielle InserJeunes
+            </Link>
+          </Box>
+          <Box mx={isMobile ? "0" : "1"}>
+            <Link
+              href="https://travail-emploi.gouv.fr/"
+              target="_blank"
+              rel="noreferrer"
+              textDecoration="underline"
+              fontWeight="semibold"
+              color="purple.900"
+            >
+              Ministère du travail
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        w="100%"
+        justifyContent={isMobile ? "center" : "flex-start"}
+        alignItems={isMobile ? "center" : "flex-start"}
+        my="2"
+      >
+        <Link href="/mentions-legales" fontSize={isMobile ? "xs" : "inherit"}>
+          Mentions légales
+        </Link>
+        <Text mx={isMobile ? "1" : "4"}>|</Text>
+        <Link href="/cgu" fontSize={isMobile ? "xs" : "inherit"}>
+          CGU
+        </Link>
+        <Text mx={isMobile ? "1" : "4"}>|</Text>
+        <Link href="/politique-confidentialite" fontSize={isMobile ? "xs" : "inherit"}>
+          Politique de confidentialité
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
--- ui/src/Components/Form/Hero.js
+++ ui/src/Components/Form/Hero.js
@@ -1,163 +1,167 @@
 import { Box, Stack, Image, Text, Button } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import shareTemoignage from "../../assets/images/share_temoignage.svg";
+import Footer from "../Footer";
 
 const Hero = ({ setStartedAnswering, isMobile }) => {
   return (
-    <Stack direction="column" w="100%" m="auto">
-      <Box w="80%" mx="auto" my="5">
-        <Stack direction={isMobile ? "column" : "row"}>
-          <Box w={isMobile ? "100%" : "50%"}>
-            <Image src={shareTemoignage} alt="" objectFit="contain" w="80%" m="auto" />
-            <Text
-              color="purple.900"
-              fontSize="xs"
-              textAlign="center"
-              mt="2"
-              display={isMobile ? "none" : "inherit"}
-            >
-              Chloé (3e) - Nazir (3e prépa-métier) - Alba (3e SEGPA)
-            </Text>
-          </Box>
-          <Box w={isMobile ? "100%" : "50%"}>
-            <Box display="flex" flexDirection={isMobile ? "column-reverse" : "column"}>
+    <>
+      <Stack direction="column" w="100%" m="auto">
+        <Box w="80%" mx="auto" my="5">
+          <Stack direction={isMobile ? "column" : "row"}>
+            <Box w={isMobile ? "100%" : "50%"}>
+              <Image src={shareTemoignage} alt="" objectFit="contain" w="80%" m="auto" />
               <Text
-                fontSize="2xl"
                 color="purple.900"
-                fontWeight="semibold"
-                textAlign={isMobile ? "center" : "inherit"}
-              >
-                Aide ces jeunes à préciser leurs choix d’orientation en répondant à leurs questions
-                sur ta formation en apprentissage
-              </Text>
-              <Text
-                fontSize="lg"
-                color="purple.900"
-                mt="5"
-                mb={isMobile ? "5" : "0"}
-                textAlign={isMobile ? "center" : "inherit"}
-              >
-                Répondre à ce questionnaire te prendra entre 5 et 10 minutes et ton anonymat sera
-                respecté.
-              </Text>
-            </Box>
-            <Button
-              size="lg"
-              variant="solid"
-              colorScheme="purple"
-              rightIcon={<ArrowForwardIcon />}
-              mt="10"
-              onClick={() => setStartedAnswering(true)}
-            >
-              Je partage mon témoignage
-            </Button>
-          </Box>
-        </Stack>
-      </Box>
-      <Box bgColor="white" w="100%" pt="10px">
-        <Box w={isMobile ? "90%" : "50%"} mx="auto" my="35">
-          <Text fontWeight="bold" color="purple.900" fontSize="xl" textAlign="center">
-            Flemme de répondre ?
-          </Text>
-          <Text color="purple.900" fontSize="xl" textAlign="center">
-            3 raisons pour te convaincre
-          </Text>
-          <Stack
-            direction={isMobile ? "column" : "row"}
-            spacing="8"
-            my="5"
-            alignItems={isMobile ? "center" : "inherit"}
-            display="flex"
-          >
-            <Box
-              bgColor="purple.200"
-              shadow="md"
-              width={isMobile ? "90%" : "250px"}
-              borderRadius="12"
-              display="flex"
-              flexDirection="column"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <Text fontSize="5xl" my="20px">
-                💬
-              </Text>
-              <Text
-                fontSize="md"
-                lineHeight="6"
-                color="purple.900"
+                fontSize="xs"
                 textAlign="center"
-                mb="20px"
-                mx="15px"
+                mt="2"
+                display={isMobile ? "none" : "inherit"}
               >
-                Personne n’est plus qualifié que toi pour
-                <b> renseigner les collégien·nes sur l’apprentissage</b>
+                Chloé (3e) - Nazir (3e prépa-métier) - Alba (3e SEGPA)
               </Text>
             </Box>
-            <Box
-              bgColor="purple.200"
-              shadow="md"
-              width={isMobile ? "90%" : "250px"}
-              borderRadius="12"
-              display="flex"
-              flexDirection="column"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <Text fontSize="5xl" my="20px">
-                👷🏻‍♀️️
-              </Text>
-              <Text
-                fontSize="md"
-                lineHeight="6"
-                color="purple.900"
-                textAlign="center"
-                mb="20px"
-                mx="15px"
+            <Box w={isMobile ? "100%" : "50%"}>
+              <Box display="flex" flexDirection={isMobile ? "column-reverse" : "column"}>
+                <Text
+                  fontSize="2xl"
+                  color="purple.900"
+                  fontWeight="semibold"
+                  textAlign={isMobile ? "center" : "inherit"}
+                >
+                  Aide ces jeunes à préciser leurs choix d’orientation en répondant à leurs
+                  questions sur ta formation en apprentissage
+                </Text>
+                <Text
+                  fontSize="lg"
+                  color="purple.900"
+                  mt="5"
+                  mb={isMobile ? "5" : "0"}
+                  textAlign={isMobile ? "center" : "inherit"}
+                >
+                  Répondre à ce questionnaire te prendra entre 5 et 10 minutes et ton anonymat sera
+                  respecté.
+                </Text>
+              </Box>
+              <Button
+                size="lg"
+                variant="solid"
+                colorScheme="purple"
+                rightIcon={<ArrowForwardIcon />}
+                mt="10"
+                onClick={() => setStartedAnswering(true)}
               >
-                <b>On te propose un espace pour t’exprimer</b>, faire le bilan de ton expérience
-                d’apprenti·e
-              </Text>
+                Je partage mon témoignage
+              </Button>
             </Box>
-            <Box
-              bgColor="purple.200"
-              shadow="md"
-              width={isMobile ? "90%" : "250px"}
-              borderRadius="12"
-              display="flex"
-              flexDirection="column"
-              justifyContent="flex-start"
-              alignItems="center"
-            >
-              <Text fontSize="5xl" my="20px">
-                👨🏽‍🎓
-              </Text>
-              <Text
-                fontSize="md"
-                lineHeight="6"
-                color="purple.900"
-                textAlign="center"
-                mb="20px"
-                mx="15px"
-              >
-                Avec ton témoignage, tu participes à <b>l’amélioration de ta formation en CFA</b>
-              </Text>
-            </Box>
-            <Button
-              size="lg"
-              variant="solid"
-              colorScheme="purple"
-              rightIcon={<ArrowForwardIcon />}
-              mt="10"
-              onClick={() => setStartedAnswering(true)}
-              display={isMobile ? "inherit" : "none"}
-            >
-              Go
-            </Button>
           </Stack>
         </Box>
-      </Box>
-    </Stack>
+        <Box bgColor="white" w="100%" pt="10px">
+          <Box w={isMobile ? "90%" : "50%"} mx="auto" my="35">
+            <Text fontWeight="bold" color="purple.900" fontSize="xl" textAlign="center">
+              Flemme de répondre ?
+            </Text>
+            <Text color="purple.900" fontSize="xl" textAlign="center">
+              3 raisons pour te convaincre
+            </Text>
+            <Stack
+              direction={isMobile ? "column" : "row"}
+              spacing="8"
+              my="5"
+              alignItems={isMobile ? "center" : "inherit"}
+              display="flex"
+            >
+              <Box
+                bgColor="purple.200"
+                shadow="md"
+                width={isMobile ? "90%" : "250px"}
+                borderRadius="12"
+                display="flex"
+                flexDirection="column"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <Text fontSize="5xl" my="20px">
+                  💬
+                </Text>
+                <Text
+                  fontSize="md"
+                  lineHeight="6"
+                  color="purple.900"
+                  textAlign="center"
+                  mb="20px"
+                  mx="15px"
+                >
+                  Personne n’est plus qualifié que toi pour
+                  <b> renseigner les collégien·nes sur l’apprentissage</b>
+                </Text>
+              </Box>
+              <Box
+                bgColor="purple.200"
+                shadow="md"
+                width={isMobile ? "90%" : "250px"}
+                borderRadius="12"
+                display="flex"
+                flexDirection="column"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <Text fontSize="5xl" my="20px">
+                  👷🏻‍♀️️
+                </Text>
+                <Text
+                  fontSize="md"
+                  lineHeight="6"
+                  color="purple.900"
+                  textAlign="center"
+                  mb="20px"
+                  mx="15px"
+                >
+                  <b>On te propose un espace pour t’exprimer</b>, faire le bilan de ton expérience
+                  d’apprenti·e
+                </Text>
+              </Box>
+              <Box
+                bgColor="purple.200"
+                shadow="md"
+                width={isMobile ? "90%" : "250px"}
+                borderRadius="12"
+                display="flex"
+                flexDirection="column"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <Text fontSize="5xl" my="20px">
+                  👨🏽‍🎓
+                </Text>
+                <Text
+                  fontSize="md"
+                  lineHeight="6"
+                  color="purple.900"
+                  textAlign="center"
+                  mb="20px"
+                  mx="15px"
+                >
+                  Avec ton témoignage, tu participes à <b>l’amélioration de ta formation en CFA</b>
+                </Text>
+              </Box>
+              <Button
+                size="lg"
+                variant="solid"
+                colorScheme="purple"
+                rightIcon={<ArrowForwardIcon />}
+                mt="10"
+                onClick={() => setStartedAnswering(true)}
+                display={isMobile ? "inherit" : "none"}
+              >
+                Go
+              </Button>
+            </Stack>
+          </Box>
+        </Box>
+        <Footer />
+      </Stack>
+    </>
   );
 };
 
