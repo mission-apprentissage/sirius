0d28fa2f3130e68e40ce3e60a80c5481670de0fc
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-06-14 10:43:38 +0200

    feat(Temoignages): add duration

--- ui/src/temoignages/Components/TemoignagesTable.js
+++ ui/src/temoignages/Components/TemoignagesTable.js
@@ -13,6 +13,7 @@ import {
 } from "@chakra-ui/react";
 import { DeleteIcon, ViewIcon } from "@chakra-ui/icons";
 import ReponsesModal from "./ReponsesModal";
+import { msToTime } from "../../utils/temoignage";
 
 const TemoignagesTable = ({ temoignages }) => {
   const [selectedTemoignageToShow, setSelectedTemoignageToShow] = useState(null);
@@ -28,7 +29,7 @@ const TemoignagesTable = ({ temoignages }) => {
         <Table size="sm">
           <Thead>
             <Tr>
-              <Th>Id</Th>
+              <Th>Durée</Th>
               <Th>Date de création</Th>
               <Th>Date mis à jour</Th>
               <Th># de réponses</Th>
@@ -36,54 +37,59 @@ const TemoignagesTable = ({ temoignages }) => {
             </Tr>
           </Thead>
           <Tbody>
-            {temoignages?.map((temoignage, index) => (
-              <Tr key={temoignage._id}>
-                <Td>{temoignage._id}</Td>
-                <Td>
-                  <Tooltip
-                    label={new Date(temoignage.createdAt).toLocaleString("fr-FR", {
-                      timeZone: "Europe/Paris",
-                    })}
-                    hasArrow
-                    arrowSize={15}
-                  >
-                    {new Date(temoignage.createdAt).toLocaleString("fr-FR")}
-                  </Tooltip>
-                </Td>
-                <Td>
-                  <Tooltip
-                    label={new Date(temoignage.updatedAt).toLocaleString("fr-FR", {
-                      timeZone: "Europe/Paris",
-                    })}
-                    hasArrow
-                    arrowSize={15}
-                  >
-                    {new Date(temoignage.updatedAt).toLocaleString("fr-FR")}
-                  </Tooltip>
-                </Td>
-                <Td>{Object.keys(temoignage.reponses).length}</Td>
-                <Td>
-                  <IconButton
-                    aria-label="Voir le témoignage"
-                    variant="outline"
-                    colorScheme="purple"
-                    icon={<ViewIcon />}
-                    mx={2}
-                    onClick={() => {
-                      setSelectedTemoignageToShow(temoignage.reponses);
-                      onOpenReponses();
-                    }}
-                  />
-                  <IconButton
-                    aria-label="Supprimer le témoignage"
-                    variant="outline"
-                    colorScheme="purple"
-                    icon={<DeleteIcon />}
-                    mx={2}
-                  />
-                </Td>
-              </Tr>
-            ))}
+            {temoignages?.map((temoignage) => {
+              const duration = msToTime(
+                new Date(temoignage.updatedAt).getTime() - new Date(temoignage.createdAt).getTime()
+              );
+              return (
+                <Tr key={temoignage._id}>
+                  <Td>{duration}</Td>
+                  <Td>
+                    <Tooltip
+                      label={new Date(temoignage.createdAt).toLocaleString("fr-FR", {
+                        timeZone: "Europe/Paris",
+                      })}
+                      hasArrow
+                      arrowSize={15}
+                    >
+                      {new Date(temoignage.createdAt).toLocaleString("fr-FR")}
+                    </Tooltip>
+                  </Td>
+                  <Td>
+                    <Tooltip
+                      label={new Date(temoignage.updatedAt).toLocaleString("fr-FR", {
+                        timeZone: "Europe/Paris",
+                      })}
+                      hasArrow
+                      arrowSize={15}
+                    >
+                      {new Date(temoignage.updatedAt).toLocaleString("fr-FR")}
+                    </Tooltip>
+                  </Td>
+                  <Td>{Object.keys(temoignage.reponses).length}</Td>
+                  <Td>
+                    <IconButton
+                      aria-label="Voir le témoignage"
+                      variant="outline"
+                      colorScheme="purple"
+                      icon={<ViewIcon />}
+                      mx={2}
+                      onClick={() => {
+                        setSelectedTemoignageToShow(temoignage.reponses);
+                        onOpenReponses();
+                      }}
+                    />
+                    <IconButton
+                      aria-label="Supprimer le témoignage"
+                      variant="outline"
+                      colorScheme="purple"
+                      icon={<DeleteIcon />}
+                      mx={2}
+                    />
+                  </Td>
+                </Tr>
+              );
+            })}
           </Tbody>
         </Table>
       </TableContainer>
