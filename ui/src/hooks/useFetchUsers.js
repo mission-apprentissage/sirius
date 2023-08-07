38186476c0524ac640975f13cf098269e65197a4
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-08-07 16:39:03 +0200

    feat(User): add useFetchUsers hook

---
+++ ui/src/hooks/useFetchUsers.js
@@ -0,0 +1,28 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { _get } from "../utils/httpClient";
+
+const useFetchUsers = (refetchData) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userContext] = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await _get(`/api/users/`, userContext.token);
+        setData(response);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [refetchData]);
+
+  return [data, loading, error];
+};
+
+export default useFetchUsers;
