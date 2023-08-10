82f16bffa5f37f09ef8a9a352a802d28c088a5c9
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-08-10 16:45:24 +0200

    feat(etablissements): protect routes

--- server/src/routes/etablissements.routes.js
+++ server/src/routes/etablissements.routes.js
@@ -9,29 +9,53 @@ const {
   updateEtablissement,
 } = require("../controllers/etablissements.controller");
 const { verifyUser } = require("../middlewares/verifyUserMiddleware");
+const { isAdmin } = require("../middlewares/isAdmin");
+const { isAdminOrAllowed, TYPES } = require("../middlewares/isAdminOrAllowed");
 
 const etablissements = () => {
   const router = express.Router();
 
-  router.post("/api/etablissements/", verifyUser, validator(createEtablissementSchema), (req, res, next) => {
+  router.post("/api/etablissements/", verifyUser, isAdmin, validator(createEtablissementSchema), (req, res, next) => {
     createEtablissement(req, res, next);
   });
 
-  router.get("/api/etablissements/", verifyUser, (req, res, next) => {
-    getEtablissements(req, res, next);
-  });
+  router.get(
+    "/api/etablissements/",
+    verifyUser,
+    (req, res, next) => isAdminOrAllowed(req, next, TYPES.SIRET),
+    (req, res, next) => {
+      getEtablissements(req, res, next);
+    }
+  );
 
-  router.get("/api/etablissements/:id", verifyUser, (req, res, next) => {
-    getEtablissement(req, res, next);
-  });
+  router.get(
+    "/api/etablissements/:id",
+    verifyUser,
+    (req, res, next) => isAdminOrAllowed(req, next, TYPES.ETABLISSEMENT_ID),
+    verifyUser,
+    (req, res, next) => {
+      getEtablissement(req, res, next);
+    }
+  );
 
-  router.delete("/api/etablissements/:id", verifyUser, (req, res, next) => {
-    deleteEtablissement(req, res, next);
-  });
+  router.delete(
+    "/api/etablissements/:id",
+    (req, res, next) => isAdminOrAllowed(req, next, TYPES.ETABLISSEMENT_ID),
+    verifyUser,
+    (req, res, next) => {
+      deleteEtablissement(req, res, next);
+    }
+  );
 
-  router.put("/api/etablissements/:id", verifyUser, validator(updateEtablissementSchema), (req, res, next) => {
-    updateEtablissement(req, res, next);
-  });
+  router.put(
+    "/api/etablissements/:id",
+    verifyUser,
+    (req, res, next) => isAdminOrAllowed(req, next, TYPES.ETABLISSEMENT_ID),
+    validator(updateEtablissementSchema),
+    (req, res, next) => {
+      updateEtablissement(req, res, next);
+    }
+  );
 
   return router;
 };
