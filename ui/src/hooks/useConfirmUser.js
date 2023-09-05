e91970fcf4a6f20da111f0f3c90640bcad28ea7c
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-09-05 17:47:26 +0200

    feat(User): add useConfirmUser

---
+++ ui/src/hooks/useConfirmUser.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { _post } from "../utils/httpClient";
+
+const useConfirmUser = (token) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const sendData = async () => {
+      try {
+        const response = await _post(`/api/users/confirm/`, { token });
+        setData(response);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    if (token) {
+      sendData();
+    }
+  }, [token]);
+
+  return [data, loading, error];
+};
+
+export default useConfirmUser;
