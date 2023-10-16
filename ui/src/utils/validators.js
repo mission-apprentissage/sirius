358da47b1790fc827194ec33b7fb97c269a1b988
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-10-16 10:21:47 +0200

    fix(Validators): change password complexity regex

--- ui/src/utils/validators.js
+++ ui/src/utils/validators.js
@@ -1,3 +1,3 @@
 export const passwordComplexityRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])(?=.{8,})/;
 export const passwordComplexityMessage = `Votre mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial`;
