7e362faae22e1f15b887aaf8695943fd7616a039
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-10-23 11:34:29 +0200

    feat(pdfExport): add multiple

--- server/src/modules/pdfExport.js
+++ server/src/modules/pdfExport.js
@@ -32,6 +32,78 @@ const fillParagraph = (text, font, fontSize, maxWidth) => {
   return paragraphs.join("\n");
 };
 
+const generateMultiplePdf = async (campagnes) => {
+  if (campagnes.length === 0) {
+    throw new Error("No campaigns provided");
+  }
+
+  const existingPdfBuffer = fs.readFileSync(pdfFilePath);
+
+  const pdfDoc = await PDFDocument.load(existingPdfBuffer);
+
+  for (let i = 0; i < campagnes.length; i++) {
+    const { campagneId, campagneName } = campagnes[i];
+
+    const qrCodeData = `${config.publicUrl}/campagnes/${campagneId}`;
+    const qrCodeDataURL = await QRCode.toDataURL(qrCodeData, {
+      errorCorrectionLevel: "L",
+      width: 250,
+      color: { dark: "#000091" },
+    });
+
+    const [page] = await pdfDoc.copyPages(pdfDoc, [0]);
+
+    pdfDoc.addPage(page);
+
+    const qrCodeImage = await pdfDoc.embedPng(qrCodeDataURL);
+
+    const pageWidth = 595;
+    const pageHeight = 842;
+
+    const qrCodeWidth = 250;
+    const qrCodeHeight = 250;
+
+    const x = (pageWidth - qrCodeWidth) / 2;
+    const y = pageHeight - qrCodeHeight - 400;
+
+    page.drawImage(qrCodeImage, {
+      x,
+      y,
+      width: qrCodeWidth,
+      height: qrCodeHeight,
+    });
+
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const fontSize = 24;
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const maxWidth = 400;
+    const breakedTextLines = fillParagraph(campagneName, font, fontSize, maxWidth).split("\n");
+    const lineHeight = fontSize * 1.2;
+    let currentY = y - 30;
+
+    for (let line of breakedTextLines) {
+      const lineWidth = helveticaFont.widthOfTextAtSize(line, fontSize);
+      const centeredX = (pageWidth - lineWidth) / 2;
+      page.drawText(line, {
+        x: centeredX,
+        y: currentY,
+        size: fontSize,
+        font: font,
+        color: rgb(0, 0, 145 / 255),
+        maxWidth: maxWidth,
+      });
+      currentY -= lineHeight;
+    }
+  }
+
+  pdfDoc.removePage(0);
+
+  const modifiedPdfBytes = await pdfDoc.saveAsBase64();
+
+  return modifiedPdfBytes;
+};
+
 const generatePdf = async (campagneId, campagneName) => {
   const qrCodeData = `${config.publicUrl}/campagnes/${campagneId}`;
   const qrCodeDataURL = await QRCode.toDataURL(qrCodeData, {
@@ -58,26 +130,32 @@ const generatePdf = async (campagneId, campagneName) => {
   page.drawImage(qrCodeImage, {
     x,
     y,
-    qrCodeWidth,
-    qrCodeHeight,
+    width: qrCodeWidth,
+    height: qrCodeHeight,
   });
 
   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
   const fontSize = 24;
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  const textWidth = helveticaFont.widthOfTextAtSize(campagneName, fontSize);
+
   const maxWidth = 400;
+  const breakedTextLines = fillParagraph(campagneName, font, fontSize, maxWidth).split("\n");
+  const lineHeight = fontSize * 1.2;
+  let currentY = y - 30;
 
-  const breakedText = fillParagraph(campagneName, font, fontSize, maxWidth);
-
-  page.drawText(textWidth > maxWidth ? breakedText : campagneName, {
-    x: textWidth > maxWidth ? 98 : page.getWidth() / 2 - textWidth / 2,
-    y: y - 30,
-    size: fontSize,
-    font: font,
-    color: rgb(0, 0, 145 / 255),
-    maxWidth: 400,
-  });
+  for (let line of breakedTextLines) {
+    const lineWidth = helveticaFont.widthOfTextAtSize(line, fontSize);
+    const centeredX = (pageWidth - lineWidth) / 2;
+    page.drawText(line, {
+      x: centeredX,
+      y: currentY,
+      size: fontSize,
+      font: font,
+      color: rgb(0, 0, 145 / 255),
+      maxWidth: maxWidth,
+    });
+    currentY -= lineHeight;
+  }
 
   const modifiedPdfBytes = await pdfDoc.saveAsBase64();
 
@@ -86,4 +164,5 @@ const generatePdf = async (campagneId, campagneName) => {
 
 module.exports = {
   generatePdf,
+  generateMultiplePdf,
 };
