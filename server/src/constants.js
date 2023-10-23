af86f894bf7c29118a7cfabfb0dc9541a4a47b9e
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-10-23 11:45:18 +0200

    feat(constants): add diplome_matcher

--- server/src/constants.js
+++ server/src/constants.js
@@ -9,4 +9,17 @@ const USER_STATUS = {
   INACTIVE: "INACTIVE",
 };
 
-module.exports = { USER_ROLES, USER_STATUS };
+const DIPLOME_TYPE_MATCHER = {
+  "CERTIFICAT D'APTITUDES PROFESSIONNELLES": "Certificat d’Aptitude Professionnelle",
+  "MENTION COMPLEMENTAIRE": "Mention Complémentaire",
+  "BREVET PROFESSIONNEL": "Brevet Professionnel",
+  "BAC PROFESSIONNEL": "Bac Professionnel",
+  "TH DE NIV 5 DES CHAMBRES DE METIERS": "Titre Homologué de Niveau 5 des Chambres de Métiers",
+  "TH DE NIV 4 DES CHAMBRES DE METIERS": "Titre Homologué de Niveau 4 des Chambres de Métiers",
+  "TH DE NIV 4 ORGANISMES GESTIONNAIRES DIVERS": "Titre Homologué de Niveau 4 Organismes Gestionnaires Divers",
+  "TH DE NIV 4 MINISTERE DU TRAVAIL - AFPA": "Titre Homologué de Niveau 4 Ministère du Travail - AFPA",
+  "TH DE NIV 5 MINISTERE DU TRAVAIL - AFPA": "Titre Homologué de Niveau 5 Ministère du Travail - AFPA",
+  "AUTRES DIPLOMES DE NIVEAU IV": "Autres Diplômes de Niveau 4",
+};
+
+module.exports = { USER_ROLES, USER_STATUS, DIPLOME_TYPE_MATCHER };
