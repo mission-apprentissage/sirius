99a901b46369fddb6cf9de0d33458b8138c65fbe
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-10-25 11:41:08 +0200

    feat(temoignages): add lastQuestionAt

--- server/src/validators/temoignages.validators.js
+++ server/src/validators/temoignages.validators.js
@@ -3,10 +3,12 @@ const Joi = require("joi");
 const createTemoignageSchema = Joi.object({
   campagneId: Joi.string().required(),
   reponses: Joi.object().required(),
+  lastQuestionAt: Joi.date().required(),
 });
 
 const updateTemoignageSchema = Joi.object({
   reponses: Joi.object().required(),
+  lastQuestionAt: Joi.date().required(),
 });
 
 module.exports = { createTemoignageSchema, updateTemoignageSchema };
