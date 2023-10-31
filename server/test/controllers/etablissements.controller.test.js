b0bb48edd6da0964dc9808a56a34c9f633d0caaa
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-10-31 17:07:10 +0100

    test(etablissement): omport stub directly

--- server/test/controllers/etablissements.controller.test.js
+++ server/test/controllers/etablissements.controller.test.js
@@ -2,8 +2,6 @@ const { use, expect } = require("chai");
 const { stub, restore } = require("sinon");
 const sinonChai = require("sinon-chai");
 const { mockRequest, mockResponse } = require("mock-req-res");
-
-const sinon = require("sinon");
 const {
   createEtablissement,
   getEtablissements,
@@ -36,7 +34,7 @@ describe(__filename, () => {
 
     it("should create a new etablissement and return it with a 201 status code", async () => {
       const expectedResponse = { id: 1, name: "Test Etablissement" };
-      sinon.stub(etablissementsService, "createEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "createEtablissement").resolves({ success: true, body: expectedResponse });
 
       await createEtablissement(req, res, next);
 
@@ -46,7 +44,7 @@ describe(__filename, () => {
     });
 
     it("should throw an EtablissementAlreadyExistingError if the etablissement already exists", async () => {
-      sinon.stub(etablissementsService, "createEtablissement").resolves({
+      stub(etablissementsService, "createEtablissement").resolves({
         success: false,
         body: { message: ErrorMessage.EtablissementAlreadyExistingError },
       });
@@ -57,7 +55,7 @@ describe(__filename, () => {
     });
 
     it("should throw a BasicError if there is an error creating the etablissement", async () => {
-      sinon.stub(etablissementsService, "createEtablissement").resolves({ success: false });
+      stub(etablissementsService, "createEtablissement").resolves({ success: false });
 
       await createEtablissement(req, res, next);
 
@@ -72,7 +70,7 @@ describe(__filename, () => {
         { id: 1, name: "Test Etablissement 1" },
         { id: 2, name: "Test Etablissement 2" },
       ];
-      sinon.stub(etablissementsService, "getEtablissements").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "getEtablissements").resolves({ success: true, body: expectedResponse });
       req.query = {};
 
       await getEtablissements(req, res, next);
@@ -83,7 +81,7 @@ describe(__filename, () => {
     });
 
     it("should throw a BasicError if there is an error getting the etablissements", async () => {
-      sinon.stub(etablissementsService, "getEtablissements").resolves({ success: false });
+      stub(etablissementsService, "getEtablissements").resolves({ success: false });
       req.query = {};
 
       await getEtablissements(req, res, next);
@@ -96,7 +94,7 @@ describe(__filename, () => {
 
     it("should return the etablissement with a 200 status code if it exists", async () => {
       const expectedResponse = { id: 1, name: "Test Etablissement" };
-      sinon.stub(etablissementsService, "getEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "getEtablissement").resolves({ success: true, body: expectedResponse });
 
       await getEtablissement(req, res, next);
 
@@ -106,7 +104,7 @@ describe(__filename, () => {
     });
 
     it("should throw a BasicError if the etablissement does not exist", async () => {
-      sinon.stub(etablissementsService, "getEtablissement").resolves({ success: false });
+      stub(etablissementsService, "getEtablissement").resolves({ success: false });
 
       await getEtablissement(req, res, next);
 
@@ -118,7 +116,7 @@ describe(__filename, () => {
 
     it("should delete an etablissement and return it with a 200 status code", async () => {
       const expectedResponse = { modifiedCount: 1 };
-      sinon.stub(etablissementsService, "deleteEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "deleteEtablissement").resolves({ success: true, body: expectedResponse });
 
       await deleteEtablissement(req, res, next);
 
@@ -128,7 +126,7 @@ describe(__filename, () => {
     });
 
     it("should throw a BasicError if there is an error deleting the etablissement", async () => {
-      sinon.stub(etablissementsService, "deleteEtablissement").resolves({ success: false });
+      stub(etablissementsService, "deleteEtablissement").resolves({ success: false });
 
       await deleteEtablissement(req, res, next);
 
@@ -137,7 +135,7 @@ describe(__filename, () => {
 
     it("should throw an EtablissementNotFoundError if the etablissement does not exist", async () => {
       const expectedResponse = { modifiedCount: 0 };
-      sinon.stub(etablissementsService, "deleteEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "deleteEtablissement").resolves({ success: true, body: expectedResponse });
 
       await deleteEtablissement(req, res, next);
 
@@ -149,7 +147,7 @@ describe(__filename, () => {
 
     it("should update an existing etablissement and return it with a 200 status code", async () => {
       const expectedResponse = { modifiedCount: 1 };
-      sinon.stub(etablissementsService, "updateEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "updateEtablissement").resolves({ success: true, body: expectedResponse });
 
       await updateEtablissement(req, res, next);
 
@@ -159,7 +157,7 @@ describe(__filename, () => {
     });
 
     it("should throw a BasicError if there is an error updating the etablissement", async () => {
-      sinon.stub(etablissementsService, "updateEtablissement").resolves({ success: false });
+      stub(etablissementsService, "updateEtablissement").resolves({ success: false });
 
       await updateEtablissement(req, res, next);
 
@@ -168,7 +166,7 @@ describe(__filename, () => {
 
     it("should throw an EtablissementNotFoundError if the etablissement does not exist", async () => {
       const expectedResponse = { modifiedCount: 0 };
-      sinon.stub(etablissementsService, "updateEtablissement").resolves({ success: true, body: expectedResponse });
+      stub(etablissementsService, "updateEtablissement").resolves({ success: true, body: expectedResponse });
 
       await updateEtablissement(req, res, next);
 
