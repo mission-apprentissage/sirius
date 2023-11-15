1fc4403ded344cded7cce8882ca4c01c37b49201
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-11-15 15:10:33 +0100

    test(verbatims): add services tests

--- server/src/services/campagnes.service.js
+++ server/src/services/campagnes.service.js
@@ -140,7 +140,6 @@ const getMultipleExport = async (ids, user) => {
       body: { data: generatedPdf, fileName },
     };
   } catch (error) {
-    console.log({ error });
     return { success: false, body: error };
   }
 };
