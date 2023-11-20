d11153d18cbbb1605666955c1c907634dc3d4946
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-11-20 15:44:25 +0100

    fix(user): remove tlds validation in login validator

--- server/src/validators/users.validators.js
+++ server/src/validators/users.validators.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().email({ tlds: false }).required(),
   password: Joi.string().required(),
 });
 
