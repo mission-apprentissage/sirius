37b210b9d8e4a05a2178e3fc4061dc99b127db87
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-11-28 17:46:55 +0100

    feat(campagnes): cascading deletion

--- server/src/services/campagnes.service.js
+++ server/src/services/campagnes.service.js
@@ -3,6 +3,7 @@ const ObjectId = require("mongoose").mongo.ObjectId;
 const campagnesDao = require("../dao/campagnes.dao");
 const formationsDao = require("../dao/formations.dao");
 const etablissementsDao = require("../dao/etablissements.dao");
+const temoignagesDao = require("../dao/temoignages.dao");
 
 const { getChampsLibreRate } = require("../utils/verbatims.utils");
 const { getMedianDuration } = require("../utils/campagnes.utils");
@@ -45,8 +46,15 @@ const createCampagne = async (campagne) => {
 
 const deleteCampagnes = async (ids) => {
   try {
-    const campagne = await campagnesDao.deleteMany(ids);
-    return { success: true, body: campagne };
+    const deletedCampagnes = await campagnesDao.deleteMany(ids);
+    await temoignagesDao.deleteManyByCampagneId(ids);
+    const deletedFormationsIds = await formationsDao.deleteManyByCampagneIdAndReturnsTheDeletedFormationId(ids);
+
+    const deletedFormationsStringifiedIds = deletedFormationsIds.map((id) => id.toString());
+
+    await etablissementsDao.updateByFormationIds(deletedFormationsStringifiedIds);
+
+    return { success: true, body: deletedCampagnes };
   } catch (error) {
     return { success: false, body: error };
   }
