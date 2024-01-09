f89b801a82134336e5a5ac9f21ecb87474540ae6
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-01-09 15:18:35 +0100

    feat(Moderation): add EtablissementPicker

--- ui/src/verbatims/ModerationPage.js
+++ ui/src/verbatims/ModerationPage.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { VStack, Spinner, Box, Text, HStack } from "@chakra-ui/react";
+import { useSearchParams } from "react-router-dom";
 import { useGet } from "../common/hooks/httpHooks";
 import ModerationTable from "./Moderation/ModerationTable";
 import ModerationStatistics from "./Moderation/ModerationStatistics";
 import useFetchVerbatims from "../hooks/useFetchVerbatims";
+import ModerationEtablissementPicker from "./Moderation/ModerationEtablissementPicker";
 
 const ModerationPage = () => {
   const [questionnaireIds, setQuestionnaireIds] = useState([]);
   const [displayedVerbatims, setDisplayedVerbatims] = useState([]);
+  const [etablissements, setEtablissements] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const [questionnaires, loadingQuestionnaires, errorQuestionnaires] =
     useGet(`/api/questionnaires/`);
@@ -23,9 +27,31 @@ const ModerationPage = () => {
 
   useEffect(() => {
     if (!loadingVerbatims && !errorVerbatims && verbatims?.length) {
+      const etablissements = verbatims.map((verbatim) => ({
+        label: verbatim.etablissement,
+        value: verbatim.etablissementSiret,
+      }));
+      const deduplicatedEtablissements = etablissements.filter(
+        (etablissement, index, self) =>
+          index === self.findIndex((t) => t.value === etablissement.value)
+      );
+
       setDisplayedVerbatims(verbatims);
+      setEtablissements(deduplicatedEtablissements);
     }
   }, [verbatims]);
+
+  useEffect(() => {
+    const etablissementSiret = searchParams.get("etablissement");
+    if (verbatims && etablissementSiret && etablissementSiret !== "all") {
+      const filteredVerbatims = verbatims.filter(
+        (verbatim) => verbatim.etablissementSiret === etablissementSiret
+      );
+      setDisplayedVerbatims(filteredVerbatims);
+    } else if (verbatims && etablissementSiret === "all") {
+      setDisplayedVerbatims(verbatims);
+    }
+  }, [searchParams, verbatims]);
 
   if (loadingQuestionnaires || errorQuestionnaires || loadingVerbatims || errorVerbatims)
     return <Spinner />;
@@ -38,7 +64,9 @@ const ModerationPage = () => {
         </Text>
       </Box>
       <ModerationStatistics verbatims={displayedVerbatims} />
-      <HStack w="100%" my="5"></HStack>
+      <HStack w="100%" my="5">
+        <ModerationEtablissementPicker etablissements={etablissements} />
+      </HStack>
       <ModerationTable verbatims={displayedVerbatims} />
     </VStack>
   );
