737761f656e327691749301d2a15a8d08b886ff7
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-02-21 11:55:21 +0100

    feat(LoginModal): rework modals with dsfr

--- ui/src/utils/validators.js
+++ ui/src/utils/validators.js
@@ -1,3 +1,13 @@
 export const passwordComplexityRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])(?=.{8,})/;
 export const passwordComplexityMessage = `Votre mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial`;
+
+export const eightCharactersRegex = /^.{8,}$/;
+
+export const oneUppercase = /^(?=.*[A-Z])/;
+
+export const oneLowercase = /^(?=.*[a-z])/;
+
+export const oneDigit = /^(?=.*[0-9])/;
+
+export const oneSpecialCharacter = /^(?=.*[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])/;
