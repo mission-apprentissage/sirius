9849b46e71d17a74f249b87df41bb823e8ff76ad
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-02-22 11:11:04 +0100

    feat(DsfrNavbar): adapt quick access

--- ui/src/Components/DsfrNavbar.js
+++ ui/src/Components/DsfrNavbar.js
@@ -1,10 +1,83 @@
 import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { Header } from "@codegouvfr/react-dsfr/Header";
 import Logo from "../assets/images/logo.svg";
 import { UserContext } from "../context/UserContext";
 
 const DsfrNavbar = () => {
+  const location = useLocation();
   const [userContext] = useContext(UserContext);
+
+  const isLandingRoute = location.pathname === "/";
+  const isLoginRoute = location.pathname === "/connexion";
+  const isSignupRoute = location.pathname === "/inscription";
+
+  const quickAccessItemsLoggedIn = [
+    {
+      iconId: "fr-icon-account-circle-fill",
+      linkProps: {
+        href: "/campagnes/gestion",
+      },
+      text: "Accéder à la plateforme",
+    },
+  ];
+
+  const quickAccessItemsLoggedOutAndLandingRoute = [
+    {
+      iconId: "fr-icon-account-circle-fill",
+      linkProps: {
+        href: "/connexion",
+      },
+      text: "Se connecter à la plateforme",
+    },
+  ];
+
+  const quickAccessItemsLoggedOutAndLoginRoute = [
+    {
+      linkProps: {
+        href: "#",
+      },
+      text: "Pas de compte ?",
+    },
+    {
+      iconId: "fr-icon-account-circle-fill",
+      linkProps: {
+        href: "/inscription",
+      },
+      text: "S'inscrire à la plateforme",
+    },
+  ];
+
+  const quickAccessItemsLoggedOutAndSignupRoute = [
+    {
+      linkProps: {
+        href: "#",
+      },
+      text: "Déjà inscrit ?",
+    },
+    {
+      iconId: "fr-icon-account-circle-fill",
+      linkProps: {
+        href: "/connexion",
+      },
+      text: "Se connecter à la plateforme",
+    },
+  ];
+
+  const quickAccessItems = () => {
+    if (userContext.token) {
+      return quickAccessItemsLoggedIn;
+    }
+    if (isLandingRoute) {
+      return quickAccessItemsLoggedOutAndLandingRoute;
+    }
+    if (isLoginRoute) {
+      return quickAccessItemsLoggedOutAndLoginRoute;
+    }
+    if (isSignupRoute) {
+      return quickAccessItemsLoggedOutAndSignupRoute;
+    }
+  };
 
   return (
     <Header
@@ -24,23 +97,7 @@ const DsfrNavbar = () => {
         imgUrl: Logo,
         orientation: "horizontal",
       }}
-      quickAccessItems={[
-        userContext.token
-          ? {
-              iconId: "fr-icon-account-circle-fill",
-              linkProps: {
-                href: "/campagnes/gestion",
-              },
-              text: "Accéder à la plateforme",
-            }
-          : {
-              iconId: "fr-icon-account-circle-fill",
-              linkProps: {
-                href: "/connexion",
-              },
-              text: "Se connecter à la plateforme",
-            },
-      ]}
+      quickAccessItems={quickAccessItems()}
     />
   );
 };
