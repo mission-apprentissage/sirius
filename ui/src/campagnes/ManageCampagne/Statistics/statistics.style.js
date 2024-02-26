40d5f8a242e00451bac6ac78694d8d9f73ae4c4b
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-02-26 14:51:26 +0100

    feat(Campagnes): rework statistics header

---
+++ ui/src/campagnes/ManageCampagne/Statistics/statistics.style.js
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import { fr } from "@codegouvfr/react-dsfr";
+
+export const StatisticsContainer = styled.header`
+  margin: 0 auto;
+  padding: ${fr.spacing("6w")} 0;
+  background-color: var(--background-alt-blue-france);
+`;
+
+export const Content = styled.div`
+  max-width: 78em;
+  margin: auto;
+  padding: 0 1.5rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  & h1 {
+    margin: 0;
+  }
+
+  & div {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+`;
+
+export const StatisticsCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 16%;
+
+  ${fr.breakpoints.down("md")} {
+    width: 50%;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
