ada7cce1ec2474227eaa8489043b71d4dc85f3ae
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-02-27 12:23:55 +0100

    feat(CellInputSeats): migrate to DSFr

---
+++ ui/src/campagnes/CellInput/CellInputSeats.js
@@ -0,0 +1,95 @@
+import React, { useState, useRef, useEffect } from "react";
+import { fr } from "@codegouvfr/react-dsfr";
+import { NotEditingContainer, StyledInput } from "./cellInput.style";
+
+const CellInputSeats = ({ id, name, campagne, handleCellUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(campagne[name]);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isFail, setIsFail] = useState(false);
+  const ref = useRef(null);
+
+  const submitHandler = async () => {
+    const { _id, nomCampagne, startDate, endDate, seats, questionnaireId } = campagne;
+
+    const result = await handleCellUpdate(_id, {
+      nomCampagne,
+      startDate,
+      endDate,
+      seats,
+      questionnaireId,
+      [name]: value ? parseInt(value) : 0,
+    });
+    if (result.status === "success") {
+      setIsSuccess(true);
+      setTimeout(() => {
+        setIsEditing(false);
+        setIsSuccess(false);
+      }, 800);
+      setValue(value);
+    } else {
+      setIsFail(true);
+      setValue(campagne[name]);
+      setTimeout(() => {
+        setIsEditing(false);
+        setIsFail(false);
+      }, 800);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        submitHandler();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [value]);
+
+  const seatsUnlimitedValue = value == "0" || !value ? "Illimité" : value;
+
+  const iconId = isSuccess
+    ? "fr-icon-success-line"
+    : isFail
+    ? "fr-icon-error-line"
+    : "fr-icon-pencil-line";
+
+  const state = isSuccess ? "success" : isFail ? "error" : "default";
+
+  return isEditing ? (
+    <StyledInput
+      iconId={iconId}
+      state={state}
+      stateRelatedMessage=""
+      ref={ref}
+      nativeInputProps={{
+        id: id,
+        name: name,
+        placeholder: "Illimité",
+        value: value == "0" ? "Illimité" : value,
+        inputMode: "numeric",
+        pattern: "[0-9]*",
+        type: "number",
+        step: 1,
+        min: 0,
+        max: 150,
+        onChange: (e) => setValue(e.target?.value),
+        onKeyDown: (e) => {
+          if (e.key === "Enter") {
+            return submitHandler();
+          }
+        },
+      }}
+    />
+  ) : (
+    <NotEditingContainer onClick={() => setIsEditing(true)}>
+      <p>{seatsUnlimitedValue}</p>
+      <span className={fr.cx("fr-icon--sm fr-icon-pencil-line")} />
+    </NotEditingContainer>
+  );
+};
+
+export default CellInputSeats;
