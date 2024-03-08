b2fc7a5930c73f94ca4084b3f78e275c986aad0b
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-03-08 11:56:46 +0100

    feat(DsfrLayout): avoid FOUC

--- ui/src/Components/DsfrLayout.js
+++ ui/src/Components/DsfrLayout.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { loadDynamicStyle, unloadStyle } from "../utils/style";
 import DsfrFooter from "./DsfrFooter";
 import DsfrNavbar from "./DsfrNavbar";
 
 const DsfrLayout = ({ children }) => {
+  const [isCssLoaded, setIsCssLoaded] = useState(false);
+
   useEffect(() => {
     const styleLink = loadDynamicStyle("/dsfr/dsfr.min.css");
+    setIsCssLoaded(true);
 
     return () => {
       unloadStyle(styleLink);
     };
   }, []);
+
+  if (!isCssLoaded) {
+    return null;
+  }
 
   return (
     <>
