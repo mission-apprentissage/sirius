ad584abbb9367e11b613d116550f51ba3be9a940
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-04-14 16:52:25 +0200

    feat(hooks): rework w/ react-query

--- ui/src/hooks/useFetchCampagnesSorted.js
+++ ui/src/hooks/useFetchCampagnesSorted.js
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { fetchCampagnesSorted } from "../queries/campagnes";
 import { useQuery } from "@tanstack/react-query";
-import { campagnesDisplayMode } from "../constants";
 
 const useFetchCampagnesSorted = (type) => {
   const [userContext] = useContext(UserContext);
@@ -10,7 +9,7 @@ const useFetchCampagnesSorted = (type) => {
   const { data, isSuccess, isError, isLoading } = useQuery({
     queryKey: [`campagnesSorted-${type}`],
     queryFn: () => fetchCampagnesSorted({ type, token: userContext.token }),
-    enabled: type === campagnesDisplayMode[0].value || type === campagnesDisplayMode[1].value,
+    enabled: !!type,
   });
 
   return { campagnesSorted: data, isSuccess, isError, isLoading };
