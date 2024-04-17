c8f07dc92d35bdf44ffe6ab297dceef47d0bbb9a
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-04-17 17:06:14 +0200

    refacto(campagne): harmonize queryKey

--- ui/src/hooks/useFetchCampagnes.js
+++ ui/src/hooks/useFetchCampagnes.js
@@ -3,12 +3,12 @@ import { UserContext } from "../context/UserContext";
 import { fetchCampagnes } from "../queries/campagnes";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-const useFetchCampagnes = ({ query, key, enabled, page, pageSize = 10 }) => {
+const useFetchCampagnes = ({ query, enabled, page, pageSize = 10 }) => {
   const queryClient = useQueryClient();
   const [userContext] = useContext(UserContext);
 
   const { data, isSuccess, isError, isLoading } = useQuery({
-    queryKey: [`campagnes-${key}`, page, query],
+    queryKey: [`campagnes`, page, query],
     queryFn: () => fetchCampagnes({ query, page, pageSize, token: userContext.token }),
     enabled: !!enabled,
   });
@@ -16,7 +16,7 @@ const useFetchCampagnes = ({ query, key, enabled, page, pageSize = 10 }) => {
   useEffect(() => {
     if (data?.pagination.hasMore) {
       queryClient.prefetchQuery({
-        queryKey: [`campagnes-${key}`, page + 1, query],
+        queryKey: [`campagnes`, page + 1, query],
         queryFn: () =>
           fetchCampagnes({ query, page: page + 1, pageSize, token: userContext.token }),
       });
--- ui/src/hooks/useFetchCampagnesSorted.js
+++ ui/src/hooks/useFetchCampagnesSorted.js
@@ -7,7 +7,7 @@ const useFetchCampagnesSorted = (type) => {
   const [userContext] = useContext(UserContext);
 
   const { data, isSuccess, isError, isLoading } = useQuery({
-    queryKey: [`campagnesSorted-${type}`],
+    queryKey: ["campagnesSorted", type],
     queryFn: () => fetchCampagnesSorted({ type, token: userContext.token }),
     enabled: !!type,
   });
