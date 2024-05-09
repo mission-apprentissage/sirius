979c5b2cad31af0e96cd395741e60907744daf09
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-05-09 18:47:09 +0200

    fix(campagnes): fix campagne statistics

---
+++ server/src/dao/verbatims.dao.js
@@ -0,0 +1,14 @@
+const Verbatim = require("../models/verbatim.model");
+
+const count = ({ query }) => {
+  return Verbatim.countDocuments({ ...query, deletedAt: null });
+};
+
+const getAll = ({ query }) => {
+  return Verbatim.find({ ...query, deletedAt: null });
+};
+
+module.exports = {
+  count,
+  getAll,
+};
