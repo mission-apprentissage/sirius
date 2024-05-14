b34136e3dfc150d67cbcfb6f06e1cefb5705d003
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-05-14 09:21:56 +0200

    feat(verbatims): add verbatim count

--- server/src/dao/verbatims.dao.js
+++ server/src/dao/verbatims.dao.js
@@ -1,8 +1,51 @@
 const ObjectId = require("mongoose").mongo.ObjectId;
 const Verbatim = require("../models/verbatim.model");
 
-const count = (query) => {
-  return Verbatim.countDocuments({ ...query, deletedAt: null });
+const count = (query = {}) => {
+  if (query.etablissementSiret) {
+    query["formation.data.etablissement_formateur_siret"] = query.etablissementSiret;
+    delete query.etablissementSiret;
+  }
+
+  if (query.formationId) {
+    query["formation._id"] = ObjectId(query.formationId);
+    delete query.formationId;
+  }
+
+  return Verbatim.aggregate([
+    {
+      $lookup: {
+        from: "temoignages",
+        localField: "temoignageId",
+        foreignField: "_id",
+        as: "temoignage",
+      },
+    },
+    {
+      $lookup: {
+        from: "formations",
+        localField: "temoignage.campagneId",
+        foreignField: "campagneId",
+        as: "formation",
+      },
+    },
+    { $unwind: "$temoignage" },
+    { $unwind: "$formation" },
+    { $match: { ...query, deletedAt: null } },
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        status: "$_id",
+        count: 1,
+      },
+    },
+  ]);
 };
 
 const getAll = ({ query }) => {
@@ -13,7 +56,7 @@ const getAllWithFormation = async (query = {}, page = 1, pageSize = 100) => {
   const offset = (page - 1) * pageSize;
 
   if (query.etablissementSiret) {
-    query["formation.data.etablissement_gestionnaire_siret"] = query.etablissementSiret;
+    query["formation.data.etablissement_formateur_siret"] = query.etablissementSiret;
     delete query.etablissementSiret;
   }
 
@@ -57,7 +100,8 @@ const getAllWithFormation = async (query = {}, page = 1, pageSize = 100) => {
               createdAt: 1,
               "formation.data.intitule_long": 1,
               "formation.data.etablissement_formateur_enseigne": 1,
-              "formation.data.etablissement_gestionnaire_siret": 1,
+              "formation.data.etablissement_formateur_entreprise_raison_sociale": 1,
+              "formation.data.etablissement_formateur_siret": 1,
             },
           },
         ],
