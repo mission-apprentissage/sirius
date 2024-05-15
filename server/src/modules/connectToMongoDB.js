8a8bd847cbf98e96f381aa87b546052151cfedff
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-05-15 17:13:43 +0200

    feat(migration): add migrateVerbatim on run

--- server/src/modules/connectToMongoDB.js
+++ server/src/modules/connectToMongoDB.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const migrateVerbatims = require("../db/migrateVerbatims");
 
 module.exports = ({ uri }) =>
   new Promise((resolve, reject) => {
@@ -6,18 +7,25 @@ module.exports = ({ uri }) =>
 
     const retry = (delay, maxRetries) => {
       mongoose.set("strictQuery", false);
-      mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async (err, client) => {
         if (err) {
           if (retries > maxRetries) {
             reject(err);
           }
 
           console.log(`Failed to connect to MongoDB - retrying in ${delay} sec`, err.message);
-
           retries++;
           setTimeout(() => retry(1000, 120), delay);
         } else {
-          resolve(client);
+          console.log("Connected to MongoDB");
+          try {
+            await migrateVerbatims({ isDryRun: false });
+            console.log("Migration executed successfully.");
+            resolve(client);
+          } catch (migrationErr) {
+            console.error("Migration failed:", migrationErr);
+            reject(migrationErr);
+          }
         }
       });
     };
