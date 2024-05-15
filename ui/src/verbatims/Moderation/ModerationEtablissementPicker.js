ef6f37306ab250f04476ccc1af6ac25165f656fb
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-05-15 10:24:28 +0200

    feat(ModerationPicker): rework to DSFR

--- ui/src/verbatims/Moderation/ModerationEtablissementPicker.js
+++ ui/src/verbatims/Moderation/ModerationEtablissementPicker.js
@@ -1,56 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { Select } from "chakra-react-select";
-import { useSearchParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Alert from "@codegouvfr/react-dsfr/Alert";
+import { Select } from "@codegouvfr/react-dsfr/SelectNext";
+import { Input } from "@codegouvfr/react-dsfr/Input";
 import useFetchLocalEtablissements from "../../hooks/useFetchLocalEtablissements";
-import { etablissementLabelGetter } from "../../utils/etablissement";
 
-const ModerationEtablissementPicker = ({ setPickedEtablissementFormationIds }) => {
-  const [allEtablissements, setAllEtablissements] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [fetchedLocalEtablissements, loading, error] = useFetchLocalEtablissements();
+const ModerationEtablissementPicker = ({ setPickedEtablissement }) => {
+  const [search, setSearch] = useState("");
+  const [inputValue, setInputValue] = useState(search);
 
-  useEffect(() => {
-    if (fetchedLocalEtablissements) {
-      setAllEtablissements(fetchedLocalEtablissements);
-    }
-  }, [fetchedLocalEtablissements]);
-
-  const matchedEtablissement =
-    allEtablissements.find(
-      (etablissement) => etablissement.data.siret === searchParams.get("etablissement")
-    ) ?? null;
+  const { localEtablissements, isSuccess, isError } = useFetchLocalEtablissements({
+    search,
+  });
 
   useEffect(() => {
-    if (
-      searchParams.get("etablissement") &&
-      searchParams.get("etablissement") !== "all" &&
-      matchedEtablissement
-    ) {
-      setPickedEtablissementFormationIds(matchedEtablissement.formationIds);
-    }
-  }, [matchedEtablissement, searchParams.get("etablissement")]);
+    const handler = setTimeout(() => {
+      setSearch(inputValue);
+    }, 500);
 
-  const onChangeHandler = (e) => {
-    setSearchParams({ etablissement: e?.data.siret ?? "all" });
-  };
+    return () => clearTimeout(handler);
+  }, [inputValue, setSearch]);
+
+  if (isError) {
+    return (
+      <Alert
+        title="Une erreur s'est produite dans la récupération des établissements"
+        description="Merci de réessayer ultérieurement"
+        severity="error"
+      />
+    );
+  }
 
   return (
-    <Select
-      id="etablissement"
-      name="etablissement"
-      variant="outline"
-      size="lg"
-      placeholder="Choix de l'établissement"
-      isSearchable
-      isClearable
-      value={matchedEtablissement}
-      isLoading={loading}
-      isDisabled={loading || !!error}
-      getOptionLabel={(option) => etablissementLabelGetter(option.data)}
-      getOptionValue={(option) => option.data.siret}
-      options={allEtablissements}
-      onChange={onChangeHandler}
-    />
+    <div>
+      <Input
+        label={`Rechercher un établissement (${localEtablissements?.length || 0})`}
+        disabled={!isSuccess || (!localEtablissements?.length && !search)}
+        nativeInputProps={{
+          value: inputValue,
+          placeholder: "Par SIRET ou nom",
+          onChange: (e) => setInputValue(e.target.value),
+        }}
+      />
+      <Select
+        label="Choix de l'établissement"
+        nativeSelectProps={{
+          onChange: (event) =>
+            setPickedEtablissement(
+              localEtablissements.find(
+                (etablissement) => etablissement.siret === event.target.value
+              )
+            ),
+        }}
+        disabled={!isSuccess || !localEtablissements?.length}
+        options={
+          (localEtablissements?.length &&
+            localEtablissements.map((etablissement) => ({
+              value: etablissement.siret,
+              label: `${etablissement.etablissement_formateur_entreprise_raison_sociale} - ${etablissement.localite} - ${etablissement.formationIds?.length} formations`,
+            }))) ||
+          []
+        }
+      />
+    </div>
   );
 };
 
