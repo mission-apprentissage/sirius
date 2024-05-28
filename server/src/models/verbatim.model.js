8e28c7df0a06f88499b3d68f7c85c8289a862d46
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-05-28 11:45:09 +0200

    feat(verbatims): adapt model to themes

--- server/src/models/verbatim.model.js
+++ server/src/models/verbatim.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { VERBATIM_STATUS } = require("../constants");
+const { VERBATIM_STATUS, VERBATIM_THEMES } = require("../constants");
 
 const gemSchema = new mongoose.Schema(
   {
@@ -21,6 +21,25 @@ const scoresSchema = new mongoose.Schema(
   { minimize: false, _id: false }
 );
 
+const themesSchema = new mongoose.Schema(
+  {
+    [VERBATIM_THEMES.INTEGRATION_AMBIANCE]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.APPRENTISSAGE_METIER]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.HORAIRES]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.RYTHME_ENTREPRISE_ETABLISSEMENT]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.MOINS_VACANCES]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.JOURNEE_TYPE_ENTREPRISE]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.DIFFICULTES_COURS]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.ENSEIGNEMENT_PROPOSE]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.EQUIPEMENTS]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.ACCESSIBILITE_HANDICAP]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.CHARGE_TRAVAIL]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.JOURNEE_TYPE_ETABLISSEMENT]: { type: Boolean, required: true, default: false },
+    [VERBATIM_THEMES.RYTHME_PERSONNEL_ETABLISSEMENT]: { type: Boolean, required: true, default: false },
+  },
+  { minimize: false, _id: false }
+);
+
 const verbatimSchema = new mongoose.Schema(
   {
     temoignageId: { type: mongoose.Schema.Types.ObjectId, required: true },
@@ -28,6 +47,7 @@ const verbatimSchema = new mongoose.Schema(
     content: { type: String, default: null },
     status: { type: String, default: VERBATIM_STATUS.PENDING },
     scores: { type: scoresSchema, default: null },
+    themes: { type: themesSchema, default: null },
     deletedAt: { type: Date, default: null },
   },
   { minimize: false, timestamps: true }
