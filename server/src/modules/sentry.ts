e589d4022d3132bc0dfaadaf9aa2bf3399c7a2f7
Antoine Bigard <bigard.antoine@gmail.com>
2024-10-23 08:54:55 +0200

    chore: metabse host + try sentry re-activation

--- server/src/modules/sentry.ts
+++ server/src/modules/sentry.ts
@@ -1,22 +1,23 @@
 import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
+// import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import config from "../config";
 
 function getOptions(): Sentry.NodeOptions {
   return {
-    tracesSampleRate: config.env === "production" ? 0.1 : 1.0,
+    dsn: config.sentry.dsn,
+    tracesSampleRate: config.env === "production" ? 0.1 : 1.0, //0.3 : 1.0,
     tracePropagationTargets: [/^https:\/\/[^/]*\.inserjeunes\.beta\.gouv\.fr/],
     profilesSampleRate: config.env === "production" ? 0.1 : 1.0,
     environment: config.env,
     release: config.version,
-    enabled: false, // config.env !== "local", TODO
+    enabled: config.env !== "local",
     integrations: [
       Sentry.httpIntegration(),
       Sentry.captureConsoleIntegration({ levels: ["error"] }),
       Sentry.extraErrorDataIntegration({ depth: 16 }),
-      // @ts-expect-error
-      nodeProfilingIntegration(),
+      // // @ts-expect-error
+      // nodeProfilingIntegration(),
       //integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
     ],
   };
@@ -29,12 +30,3 @@ export function initSentry(): void {
 export async function closeSentry(): Promise<void> {
   await Sentry.close(2_000);
 }
-
-// Sentry.init({
-//   dsn: config.sentry.dsn,
-//   integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
-//   enabled: config.env !== "local",
-//   tracesSampleRate: config.env === "production" ? 0.3 : 1.0,
-//   tracePropagationTargets: [/^https:\/\/[^/]*\.inserjeunes\.beta\.gouv\.fr/],
-//   environment: config.env,
-// });
