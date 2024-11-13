2f49e367cfff082c5ca564321c488367b611ce1c
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-11-13 17:23:11 +0100

    feat(formations): add onisep_slug column

---
+++ server/src/migrations/migration_1731512210632.ts
@@ -0,0 +1,35 @@
+import type { Kysely } from "kysely";
+import { sql } from "kysely";
+
+interface Database {
+  formations: {
+    id: string;
+    campagne_id: string | null;
+    onisep_slug: string | null;
+    onisepUrl: string | null;
+  };
+  campagnes: {
+    id: string;
+  };
+}
+
+export const up = async (db: any) => {
+  await db.schema.alterTable("formations").addColumn("onisep_slug", "varchar(10)").execute();
+
+  const formations = await db
+    .selectFrom("formations")
+    .select(["formations.id", sql<string>`catalogue_data->>'onisep_url'`.as("onisep_url")])
+    .execute();
+
+  formations.forEach(async (formation: any) => {
+    const onisep_slug = formation?.onisepUrl?.match(/FOR\.\d+/);
+
+    if (onisep_slug) {
+      await db.updateTable("formations").set({ onisep_slug: onisep_slug[0] }).where("id", "=", formation.id).execute();
+    }
+  });
+};
+
+export const down = async (db: Kysely<Database>) => {
+  await db.schema.alterTable("formations").dropColumn("onisep_slug").execute();
+};
