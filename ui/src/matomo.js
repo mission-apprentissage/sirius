2c6c846a2fd4954c30aed873c4e2e051b9f1d352
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-12-16 11:13:47 +0100

    feat(iframeFormation): add missing tracking event

--- ui/src/matomo.js
+++ ui/src/matomo.js
@@ -7,7 +7,7 @@ export const MATOMO_ACTION = {
   CLICK_CAROUSEL_PREVIOUS: "Click carousel précédent",
   CLICK_CAROUSEL_NEXT: "Click carousel suivant",
   CLICK_CAROUSEL_SEE_MORE: "Click carousel voir plus de témoignages",
-  CLICK_THEMATIC_FROM_GRAPH: "Click thématique depuis graphique", //todo
+  CLICK_THEMATIC_FROM_GRAPH: "Click thématique depuis graphique",
   CLICK_GRAPHIC_VIEW: "Click passage en mode graphique",
   CLICK_TABLE_VIEW: "Click passage en mode tableau",
   CLICK_VERBATIM_SEE_MORE: "Click voir plus",
@@ -16,4 +16,6 @@ export const MATOMO_ACTION = {
   CLICK_QUESTION: "Click question",
   CLICK_GO_BACK_THEMATICS: "Click revenir en arrière thématiques",
   CLICK_GO_BACK_QUESTIONS: "Click revenir en arrière questions",
+  CLICK_TROUVER_ENTREPRISE_SEE_MORE: "Click voir plus trouver une entreprise",
+  CLICK_TROUVER_ENTREPRISE_SEE_LESS: "Click voir moins trouver une entreprise",
 };
