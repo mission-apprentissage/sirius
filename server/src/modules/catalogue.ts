8a6eeb6694bf4ffca54165a29e847318810d3661
Yohann Gablowski <yohann.gablowski@gmail.com>
2025-01-28 17:41:37 +0100

    feat(formations): add script to renew CFD

--- server/src/modules/catalogue.ts
+++ server/src/modules/catalogue.ts
@@ -1,14 +1,24 @@
-// @ts-nocheck -- TODO
-
-const CATALOGUE_API = "https://catalogue-apprentissage.intercariforef.org/api/v1/entity/etablissements";
+const ETABLISSEMENTS_CATALOGUE_API = "https://catalogue-apprentissage.intercariforef.org/api/v1/entity/etablissements";
+const FORMATION_CATALOGUE_API = "https://catalogue-apprentissage.intercariforef.org/api/v1/entity/formation";
 
 export const getEtablissement = async (siret: string) => {
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
   const fetch = (await import("node-fetch")).default;
-  const response = await fetch(`${CATALOGUE_API}?query={ "siret": "${siret}"}&page=1&limit=1`, {
+  const response = await fetch(`${ETABLISSEMENTS_CATALOGUE_API}?query={ "siret": "${siret}"}&page=1&limit=1`, {
     method: "GET",
   });
-  const data = await response.json();
+  const data = (await response.json()) as { etablissements: any[] };
 
   return data.etablissements[0];
+};
+
+export const getFormation = async (catalogue_id: string, options = "") => {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const fetch = (await import("node-fetch")).default;
+  const response = await fetch(`${FORMATION_CATALOGUE_API}/${catalogue_id}?${options}`, {
+    method: "GET",
+  });
+
+  const data = (await response.json()) as any;
+  return data;
 };
