eae941555d48a036a6799fe1dbbefe76beb81c8b
Yohann Gablowski <yohann.gablowski@gmail.com>
2025-01-30 10:20:50 +0100

    feat(iframeFormation): use _paq

--- ui/src/hooks/useMatomoEvent.js
+++ ui/src/hooks/useMatomoEvent.js
@@ -2,15 +2,10 @@
 import { useCallback } from "react";
 
 const useMatomoEvent = () => {
-  return useCallback((category, action, name, value) => {
-    if (window._mtm) {
-      window._mtm.push({
-        event: "customEvent",
-        category,
-        action,
-        name,
-        value,
-      });
+  return useCallback((category, action, name, value, inituleFormation) => {
+    if (window._paq) {
+      window._paq.push(["setCustomDimension", 1, inituleFormation]);
+      window._paq.push(["trackEvent", category, action, name, value]);
     } else {
       console.warn("Matomo is not initialized yet.");
     }
